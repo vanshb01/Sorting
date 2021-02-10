7630ec2b8bcba8dd88174e27bf5e5b87b2113468
Vansh Bansal <bansalvansh77@gmail.com>
2021-02-10 22:08:22 +0530

    Updated Merge Sort

--- eclipse-workspace/Sorting/src/NewSort.java
+++ eclipse-workspace/Sorting/src/NewSort.java
@@ -1,36 +1,88 @@
+class NewSort 
+{
 
-public class NewSort {
-	static void newSort(int[] arr) {  
-        int n = arr.length;  
-        int temp = 0;  
-         for(int i=0; i < n; i++){  
-                 for(int j=1; j < (n-i); j++){  
-                          if(arr[j-1] > arr[j]){  
-                                 //swap elements  
-                                 temp = arr[j-1];  
-                                 arr[j-1] = arr[j];  
-                                 arr[j] = temp;  
-                         }  
-                          
-                 }  
-         }  
-  
-    }  
-    public static void main(String[] args) {  
-                int arr[] ={44, 34, 25, 10, 22, 11, 90};  
-                 
-                System.out.println("Array Before Bubble Sort");  
-                for(int i=0; i < arr.length; i++){  
-                        System.out.print(arr[i] + " ");  
-                }  
-                System.out.println();  
-                  
-                newSort(arr);//sorting array elements using bubble sort  
-                 
-                System.out.println("Array After Bubble Sort");  
-                for(int i=0; i < arr.length; i++){  
-                        System.out.print(arr[i] + " ");  
-                }  
-   
-        }  
-}  
+    void e19cse130_merge(int arr[], int l, int m, int r)
+    {
+
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+ 
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+ 
+        
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
+
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+
+            int m =l+ (r-l)/2;
+
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+ 
+
+            e19cse130_merge(arr, l, m, r);
+        }
+    }
+ 
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+
+    public static void main(String args[])
+    {
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+ 
+        System.out.println("Given Array");
+        printArray(arr);
+ 
+        NewSort ob = new NewSort();
+        ob.sort(arr, 0, arr.length - 1);
+ 
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
